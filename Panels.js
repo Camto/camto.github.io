175c8b02692336dd68b8b63b2055c674ee48a5b3
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2017-12-10 13:55:31 -0400

    First two cards.

--- Panels.js
+++ Panels.js
@@ -1,32 +1,42 @@
 $(document).ready(function() {
-	
-	for(let cou = 0; cou < 20; cou++) {
-		$("#Panels").append(`
-			<div class="Panel">
-				<div class="Panel-Margin-Box">
-					 <div class="Panel-Content-Box">
-					 	<div class="Panel-Image"></div>
-						<div class="Panel-Title">Title</div>
-						<div class="Panel-Description">Short description.</div>
-					 </div>
+	$.getJSON("Panels.json", function(Panels) {
+		
+		for(let cou = 0; cou < Panels.length; cou++) {
+			
+			let title = Panels[cou].title;
+			let image = Panels[cou].image;
+			let link = Panels[cou].link;
+			let description = Panels[cou].description;
+			
+			$("#Panels").append(`
+				<div class="Panel">
+					<div class="Panel-Margin-Box">
+						 <div class="Panel-Content-Box">
+						 	<a href="${link}"><div class="Panel-Image" style="background-image: url('${"PICS/" + image}')"></div></a>
+							<a class="Panel-Title" href="${link}">${title}</a>
+							<div class="Panel-Description">${description}</div>
+						 </div>
+					</div>
 				</div>
-			</div>
-		`);
-	}
-	resize_all();
-	
-	$(".Panel-Content-Box").mouseover(function() {
-		$(this).find(".Panel-Image").stop().animate({
-			width: $(this).find(".Panel-Image").height() * 1.51
-		}, 500, function() {});
+			`);
+			
+		}
+		
+		resize_all();
+		
+		$(".Panel-Content-Box").mouseover(function() {
+			$(this).find(".Panel-Image").stop().animate({
+				width: $(this).find(".Panel-Image").height() * 1.51
+			}, 500, function() {});
+		});
+		
+		$(".Panel-Content-Box").mouseleave(function() {
+			$(this).find(".Panel-Image").stop().animate({
+				width: $(this).find(".Panel-Image").height()
+			}, 500, function() {});
+		});
+		
 	});
-	
-	$(".Panel-Content-Box").mouseleave(function() {
-		$(this).find(".Panel-Image").stop().animate({
-			width: $(this).find(".Panel-Image").height()
-		}, 500, function() {});
-	});
-	
 });
 
 $(window).resize(resize_all);
@@ -45,13 +55,13 @@ function square_panels() {
 function resize_panel_content() {
 	
 	$(".Panel-Image").each(function() {
-		$(this).width($(this).parent().width() * 1);
-		$(this).height($(this).parent().height() * 1);
+		$(this).width($(this).parent().parent().width() * 1);
+		$(this).height($(this).parent().parent().height() * 1);
 	});
 	
 	$(".Panel-Title").each(function() {
-		$(this).width($(this).parent().width() * 2.25);
-		$(this).css("margin-top", $(this).parent().height() * 1.05);
+		$(this).width($(this).parent().parent().width() * 2.25);
+		$(this).css("margin-top", $(this).parent().parent().height() * 1.15);
 	});
 	
 	$(".Panel-Description").each(function() {
