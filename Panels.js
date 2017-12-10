a15aa98263e853497f505b4dfb678809b79dd4ee
Benjamin Philippe Applegate <www.ben.pr@gmail.com>
2017-12-10 12:20:20 -0400

    Interactive cards!

--- Panels.js
+++ Panels.js
@@ -4,19 +4,60 @@ $(document).ready(function() {
 		$("#Panels").append(`
 			<div class="Panel">
 				<div class="Panel-Margin-Box">
-					 <div class="Panel-Content-Box"></div>
+					 <div class="Panel-Content-Box">
+					 	<div class="Panel-Image"></div>
+						<div class="Panel-Title">Title</div>
+						<div class="Panel-Description">Short description.</div>
+					 </div>
 				</div>
 			</div>
 		`);
 	}
-	square_panels();
+	resize_all();
+	
+	$(".Panel-Content-Box").mouseover(function() {
+		$(this).find(".Panel-Image").stop().animate({
+			width: $(this).find(".Panel-Image").height() * 1.51
+		}, 500, function() {});
+	});
+	
+	$(".Panel-Content-Box").mouseleave(function() {
+		$(this).find(".Panel-Image").stop().animate({
+			width: $(this).find(".Panel-Image").height()
+		}, 500, function() {});
+	});
 	
 });
 
-$(window).resize(square_panels);
+$(window).resize(resize_all);
+
+function resize_all() {
+	square_panels();
+	resize_panel_content();
+}
 
 function square_panels() {
 	$(".Panel").each(function() {
 		$(this).height($(this).width());
 	});
+}
+
+function resize_panel_content() {
+	
+	$(".Panel-Image").each(function() {
+		$(this).width($(this).parent().width() * 1);
+		$(this).height($(this).parent().height() * 1);
+	});
+	
+	$(".Panel-Title").each(function() {
+		$(this).width($(this).parent().width() * 2.25);
+		$(this).css("margin-top", $(this).parent().height() * 1.05);
+	});
+	
+	$(".Panel-Description").each(function() {
+		$(this).width($(this).parent().width() * 0.75);
+		$(this).height($(this).parent().height() * 2.25);
+		$(this).css("margin-left", $(this).parent().width() * 1.45);
+	});
+	
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# My GitHub homepage!
-Just visit [camto.github.io](http://camto.github.io).
+# This is my GitHub page.
+Visit [camto.github.io](http://camto.github.io) to see my best projects! _(W.I.P.)_
